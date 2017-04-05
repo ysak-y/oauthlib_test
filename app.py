2606d7d4aab283eb2ae4c705b41eb058b81ebb36
ysak-y <yoshiaki.0614@gmail.com>
2017-04-05 23:06:55 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from oauthlib.common import Request
+from oauthlib.oauth2 import AuthorizationCodeGrant, RequestValidator, BearerToken
+import os
+
+
+class MyRequestValidator(RequestValidator):
+    def validate_client_id(self, client_id, request, *args, **kargs):
+        return True
+
+client_id = os.env['TWITTER_CLIENT_ID']
+
+uri = ''
+body = {}
+headers = headers
+
+request = Request(uri, body=body, headers=headers)
+
+request_validator = MyRequestValidator()
+beaer_token = BearerToken(request_validator)
+client = AuthorizationCodeGrant(request, beaer_token)
